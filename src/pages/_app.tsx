302492c07de91be9046587b9cdc5f6903f5d9b7b
Matheus Roika <matheusroika@hotmail.com>
2021-04-22 16:33:04 -0300

    Aula 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/global.scss'
+import '../styles/global.scss';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
 import styles from '../styles/app.module.scss'
 
-import { Header } from "../components/Header";
-import { Player } from '../components/Player';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <Header />
         <Component {...pageProps} />
       </main>
-        <Player />
+      <Player />
     </div>
   )
 }
---
+++ src/pages/episodes/[episode].tsx
@@ -0,0 +1,92 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import { api } from '../../services/api'
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString'
+
+import styles from './episode.module.scss'
+
+type Episode = {
+  id: string;      
+  title: string;
+  members: string;
+  publishedAt: string;
+  thumbnail: string;
+  durationAsString: string;
+  description: string;
+  file: {
+    url: string;
+    type: string;
+    duration: number;
+  }
+}
+
+type EpisodeProps = {
+  episode: Episode;
+}
+
+export default function Episode({ episode }: EpisodeProps) {
+  return (
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar"/>
+          </button>
+        </Link>
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"
+        />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio" />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description}}
+      />
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const episodeId = context.params.episode
+
+  const { data } = await api.get(`/episodes/${episodeId}`)
+
+  const episode = {
+    ...data,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+    durationAsString: convertDurationToTimeString(data.file.duration)
+  }
+  delete episode.published_at
+
+  return {
+    props: {
+      episode,
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  }
+}
